a27a86348f879f89540fa0686fc8857805fab916
abc6577 <terrye1992@gmail.com>
2022-10-14 19:05:43 +0800

    W06-P2: add navbar for demo links

---
+++ js/script_91.js
@@ -0,0 +1,16 @@
+function change(index){
+
+    const p = document.querySelector(',show-classdemo')
+    console,log('p',p);
+    switch(index){
+        case 1:
+            p.innerHTML = `<iframe src="./demo/w01_91/" width="100%" height="100%" />`
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+    }
+}
