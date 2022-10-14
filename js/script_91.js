9a80b7439202da887813d62ed670d657ab24a433
abc6577 <terrye1992@gmail.com>
2022-10-15 05:17:35 +0800

    W06-P3: run 4 classdemo one by one

--- js/script_91.js
+++ js/script_91.js
@@ -1,16 +1,19 @@
-function change(index){
+function change(index) {
 
-    const p = document.querySelector(',show-classdemo')
-    console,log('p',p);
-    switch(index){
+    const p = document.querySelector('.show-classdemo');
+    console.log('p', p);
+    switch(index) {
         case 1:
-            p.innerHTML = `<iframe src="./demo/w01_91/" width="100%" height="100%" />`
+            p.innerHTML = `<iframe src="./demo/w01_91/index.html" width="100%" height="100%" />`
         break;
         case 2:
+            p.innerHTML = `<iframe src="./demo/w02_91/card_91.html" width="100%" height="100%" />`
         break;
         case 3:
+            p.innerHTML = `<iframe src="./demo/w04_91/blog.html" width="100%" height="100%" />`
         break;
         case 4:
+            p.innerHTML = `<iframe src="./demo/w06_91/blog.html" width="100%" height="100%" />`
         break;
     }
 }
